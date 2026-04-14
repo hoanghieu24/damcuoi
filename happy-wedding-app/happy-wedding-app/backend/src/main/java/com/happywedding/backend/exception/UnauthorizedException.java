76f6fd315035bfca56c6ea3950d8609be67078d5
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-14 10:21:32 +0700

    Delete happy-wedding-app/happy-wedding-app/backend directory

--- happy-wedding-app/happy-wedding-app/backend/src/main/java/com/happywedding/backend/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.happywedding.backend.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
